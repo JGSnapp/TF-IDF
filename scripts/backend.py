e4fde72089b41b0e70d227d55c744117eb24121f
JGSnapp <lieutenantsnapper@gmail.com>
2025-11-08 03:12:07 +0300

    edit structure

--- scripts/backend.py
+++ scripts/backend.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel, Field
 from transformers import pipeline
+import uvicorn
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-STANDART_PATH = "../models/checkpoint-5613"
+STANDART_PATH = os.getenv("STANDART_PATH", "../models/checkpoint-5613")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://app:secret@localhost:5432/mydb")
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+pipe = pipeline("text-classification", model=STANDART_PATH, tokenizer=STANDART_PATH)
+
+def insert_request(engine, comment: str, comment_type: int, model: str):
+    sql = text("""
+        INSERT INTO requests (comment, comment_type, model)
+        VALUES (:comment, :comment_type, :model)
+    """)
+    with engine.begin() as conn:
+        conn.execute(sql, {
+            "comment": comment,
+            "comment_type": comment_type,
+            "model": model,
+        })
+
+
+def get_requests(engine):
+    sql = text("""
+        SELECT id, comment, comment_type, model, created_at
+        FROM requests
+        ORDER BY created_at DESC
+    """)
+    with engine.connect() as conn:
+        res = conn.execute(sql)
+        return res.mappings().all()
+
 
 class RequestBody(BaseModel):
     text: str = Field(..., min_length=1, max_length=512)
@@ -14,10 +47,25 @@ app = FastAPI()
 def root():
     return {"message": "Все работает!"}
 
+@app.get("/get_list")
+def get_list():
+    try:
+        return get_requests(engine)
+    except Exception as e:
+        import traceback; traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
+
 @app.post("/predict")
-def root(payload: RequestBody = Body(..., embed=True)):
-    path = payload.model
-    pipe = pipeline("text-classification", model=path, tokenizer=path, device_map="auto")
-    result = int(pipe(payload.text))
+def predict(payload: RequestBody = Body(...)):
+    model_path = payload.model
+    text_input = payload.text
+    global pipe
+    if model_path != STANDART_PATH:
+        pipe = pipeline("text-classification", model=model_path, tokenizer=model_path)
+    result = int(pipe(text_input)[0]['label'])
     variants = ["Мусор", "Негативный отзыв", "Нейтральный отзыв", "Позитивный отзыв"]
+    insert_request(engine, comment = text_input, comment_type = result, model = model_path)
     return {"message": f"Результат: {variants[result]}"}
+
+if __name__ == "__main__":
+    uvicorn.run("backend:app", host="0.0.0.0", port=1000, reload=True)
--- scripts/frontend.py
+++ scripts/frontend.py
@@ -3,7 +3,8 @@ import requests
 
 st.title("Оценка настроения комментария")
 
-api_url = "http://localhost:1000/predict"
+predict_url = "http://localhost:1000/predict"
+get_list_url = "http://localhost:1000/get_list"
 
 text = st.text_area("Введите комментарий:", height=150)
 model_path = st.text_input("Путь/ID модели (опционально)", value="../models/checkpoint-5613")
@@ -13,9 +14,15 @@ placeholder = st.empty()
 if st.button("Отправить"):
     with st.spinner("Отправляю запрос..."):
             try:
-                r = requests.post(api_url, json={"text": text, "model": model_path}, timeout=60)
+                r = requests.post(predict_url, json={"text": text, "model": model_path}, timeout=60)
                 data = r.json()
                 placeholder.success(data.get("message", "OK"))
-                st.json(data)
             except Exception as e:
                 placeholder.error(f"Не удалось отправить запрос: {e}")
+
+r = requests.get(get_list_url, timeout=60)
+r.raise_for_status()
+data = r.json()
+st.header("История запросов")
+for item in data:
+    st.write(f"- Комментарий: {item['comment']}, Тип: {item['comment_type']}, Модель: {item['model']}")
